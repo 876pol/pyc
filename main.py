00775743d1361813dc3babffa4d9060a6cf0e874
PAUL CHEN <77174148+876pol@users.noreply.github.com>
2022-04-06 01:53:11 +0000

    update everything

---
+++ main.py
@@ -0,0 +1,25 @@
+from interpreter import Interpreter
+from os import listdir, getcwd
+from sys import argv
+from lexer import Lexer
+from parser import Parser
+
+def main():
+    cwd = getcwd()
+    dir_files = listdir(cwd)
+    if len(argv) <= 1:
+        print("Error: No source file provided")
+        return
+    if argv[1] not in dir_files:
+        print("Error: No source file found")
+        return
+    file = open(argv[1], "r")
+    code = file.read()
+    lexer = Lexer(code)
+    parser = Parser(lexer)
+    interpreter = Interpreter(parser)
+    result = interpreter.interpret()
+    print(result)
+
+if __name__ == "__main__":
+    main()
